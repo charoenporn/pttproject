768a444a38876531cd0308bbe31624ee4df6b3a4
charoenporn <48587099+charoenporn@users.noreply.github.com>
2021-08-02 18:00:37 +0700

    new

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -4,17 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users extends CI_Controller {
 	public function index()
 	{
-		//$this->load->view('welcome_message');
 		$this->load->model('UserModel');
 		$tmp = $this->UserModel->get_all_users();
+		//$tmp2 = $this->UserModel->check_username_password('111','222');
+		
 //		var_dump($tmp);
+
 		$data = array('navbar_name'=>'Users');
 		$this->load->view('dashboard/top');
 		$this->load->view('dashboard/navbar',$data);
 		$this->load->view('dashboard/topcontent');
 		//$this->load->view('users/user_form');
+		//$page_data = array('userList'=>$tmp,'datatml2'=>$tmp2);
+		$page_data['userList'] = $tmp;
+		//$page_data['datatmp2'] = $tmp2;
 
-		$this->load->view('users/user_list');
+		$this->load->view('users/user_list',$page_data);
 
 		$this->load->view('dashboard/footcontent');
 		$this->load->view('dashboard/footer');
@@ -30,6 +35,23 @@ class Users extends CI_Controller {
 		$this->load->view('dashboard/navbar',$data);
 		$this->load->view('dashboard/topcontent');
 		$this->load->view('users/user_form');
+		$this->load->view('dashboard/footcontent');
+		$this->load->view('dashboard/footer');
+	}
+
+	public function edit_user()
+	{
+		//$this->load->view('welcome_message');
+		$this->load->model('UserModel');
+		$tmp = $this->UserModel->get_all_users();
+//		var_dump($tmp);
+		$data = array('navbar_name'=>'Users');
+		$this->load->view('dashboard/top');
+		$this->load->view('dashboard/navbar',$data);
+		$this->load->view('dashboard/topcontent');
+
+		$this->load->view('users/user_edit_form');
+
 		$this->load->view('dashboard/footcontent');
 		$this->load->view('dashboard/footer');
 	}
--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -2,13 +2,19 @@
 class UserModel extends CI_Model {
     public function get_all_users()
     {
-            $query = $this->db->get('users');
+            //$query = $this->db->get('users'); // SELECT * FROM users
+
+            $query = $this->db->query("SELECT * FROM users WHERE activeflag = 1");
             return $query->result();
     }
 
     public function check_username_password($username,$password)
     {
-            $query = $this->db->get('users');
+            //$query = $this->db->get('users');
+            $sql = "SELECT * FROM users WHERE username='".$username."' and password='".$password."' and activeflag = 1";
+            //echo $sql;
+            //die();
+            $query = $this->db->query($sql);
             $user = $query->result();
             if($user.sizeof()==1){
 
